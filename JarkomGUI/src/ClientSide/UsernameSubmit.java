e16717f3ab59e4247b0351ef58398098e22ca1c4
ArifandiBima <arifandi.bima@gmail.com>
2025-07-01 21:11:19 +0700

    why is this?

--- JarkomGUI/src/ClientSide/UsernameSubmit.java
+++ JarkomGUI/src/ClientSide/UsernameSubmit.java
@@ -75,6 +75,8 @@ public class UsernameSubmit extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         if(ChatClient.usernameVerified(this.usernameField.getText())){
             //generate the next screen
+            RoomSelect.makeScreen();
+            this.dispose();
         }
         else{
             this.tryAgainLabel.setText("username taken");
